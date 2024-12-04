08feceeedf207900bb756c1c3902611667243aba
Zenith <q1095568627@163.com>
2024-12-04 14:33:45 +0800

    Tauri Init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  ssr: false,
+  // Enables the development server to be discoverable by other devices when running on iOS physical devices
+  devServer: { host: process.env.TAURI_DEV_HOST || "localhost" },
+  vite: {
+    // Better support for Tauri CLI output
+    clearScreen: false,
+    envPrefix: ["VITE_", "TAURI_"],
+    server: {
+      strictPort: true,
+    },
+  },
+});
